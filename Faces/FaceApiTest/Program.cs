9ec5bc65db7f706d45a8e6249ec518b965c89291
parhambarazesh <parham.barazesh@gmail.com>
2022-01-24 13:06:13 +0100

    Add project files.

---
+++ Faces/FaceApiTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FaceApiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var imagePath = @"image2.jpg";
+            var urlAddress = "http://localhost:6001/api/faces";
+            ImageUtility imgUtil = new ImageUtility();
+            var bytes = imgUtil.ConvertToBytes(imagePath);
+            List<byte[]> faceList = null;
+            var byteContent = new ByteArrayContent(bytes);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            using (var httpClient=new HttpClient())
+            {
+                using (var response=await httpClient.PostAsync(urlAddress,byteContent))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    faceList = JsonConvert.DeserializeObject<List<byte[]>>(apiResponse);
+                }
+            }
+            if (faceList.Count>0)
+            {
+                for (int i = 0; i < faceList.Count; i++)
+                {
+                    imgUtil.FromBytesToImage(faceList[i], "faceSavedByFaceTestAPI" + i);
+                }
+            }
+        }
+    }
+}
---
+++ Faces/playbook/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace playbook
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            var urlAddress = "https://api.covid19api.com/summary";
+            var response = await httpClient.GetAsync(urlAddress);
+            var apiResponse = await response.Content.ReadAsStringAsync();
+            JObject res = JObject.Parse(apiResponse);
+            JArray data = (JArray)res["Countries"];
+            var queriedData = data.Where(c => (string)c["Country"] == "Norway");
+            Console.WriteLine(queriedData.ToList()[0]);
+        }
+    }
+}
